6934899c7e8460b119cdb69bb93e1b53e45f286c
cyb0rg14 <techtitanbot@gmail.com>
2024-06-21 16:23:49 +0530

    added submit button to store users' data

--- webapp.py
+++ webapp.py
@@ -1,27 +1,29 @@
 from datetime import datetime
-
 import streamlit as st
+import pandas as pd
+import os
 
-from constants import *
+# Import your constants from the constants module
+from constants import indian_states_and_ut
 
-# set page configuraiton
+# Set page configuration
 st.set_page_config(
     page_title="Unique Ads Campaign",
     page_icon=":bar_chart:",
 )
 
-# set homepage
+# Set homepage
 st.title("Unique Ads Campaign")
 st.error("Please fill all the details carefully!")
 
-# divide into two columns
+# Divide into two columns
 col1, col2 = st.columns(2)
 with col1:
     start_date = st.date_input("Start Date", datetime.today())
 with col2:
     end_date = st.date_input("End Date", datetime.today())
 
-# take inputs from user
+# Take inputs from user
 ad_campaign_name = st.text_input("Set a name for your ad campaign:")
 cost_per_click = st.slider(
     "Set a CPC (Cost Per Click) for your ad:", min_value=8, max_value=20
@@ -33,7 +35,7 @@ target_location = st.multiselect(
     "Select the target locations for your ad:", indian_states_and_ut
 )
 
-# more input from users about ads
+# More input from users about ads
 st.divider()
 st.markdown("### Tell us more about your ad:")
 headline = st.text_input("Set a headline for your ad:")
@@ -47,7 +49,7 @@ your_website = st.text_input(
     placeholder="www.yourwebsite.com/signup",
 )
 
-# way to connect with users
+# Way to connect with users
 st.divider()
 st.markdown("### Connect with your potential customers:")
 phone_no = st.text_input(
@@ -55,7 +57,54 @@ phone_no = st.text_input(
 )
 email = st.text_input("Enter your email:", placeholder="youremail@mail.com (Optional)")
 
-# copyright
+# Submit button
+if st.button("Submit"):
+    # Check for required fields
+    if not ad_campaign_name:
+        st.error("Ad campaign name is required.")
+    elif not daily_budget:
+        st.error("Daily budget is required.")
+    elif not target_location:
+        st.error("At least one target location is required.")
+    elif not headline:
+        st.error("Ad headline is required.")
+    elif not primary_text:
+        st.error("Primary text is required.")
+    elif not description:
+        st.error("Description is required.")
+    elif not website_url:
+        st.error("Website URL is required.")
+    elif not your_website:
+        st.error("Redirection URL is required.")
+    else:
+        # All required fields are filled, prepare to save data
+        data = {
+            "Start Date": [start_date],
+            "End Date": [end_date],
+            "Ad Campaign Name": [ad_campaign_name],
+            "Cost Per Click": [cost_per_click],
+            "Daily Budget": [daily_budget],
+            "Target Locations": [", ".join(target_location)],
+            "Headline": [headline],
+            "Primary Text": [primary_text],
+            "Description": [description],
+            "Website URL": [website_url],
+            "Redirection URL": [your_website],
+            "Phone No": [phone_no],
+            "Email": [email]
+        }
+        
+        df = pd.DataFrame(data)
+
+        # Save to CSV
+        if not os.path.exists('ads_campaigns.csv'):
+            df.to_csv('ads_campaigns.csv', index=False)
+        else:
+            df.to_csv('ads_campaigns.csv', mode='a', header=False, index=False)
+
+        st.success("Ad campaign details have been saved successfully!")
+
+# Copyright
 st.markdown("# ")
 st.markdown("# ")
 copyright = "&copy; 2024 Unique Ads Campaign. All rights reserved."
